f4c151236e62ae1cd610cb33c29de66ca2cadc95
Sulstice <sharifsuliman1@gmail.com>
2022-04-08 14:17:26 -0400

    ss-main: added depedency

--- global_chem/setup.py
+++ global_chem/setup.py
@@ -46,7 +46,16 @@ setup(
         'bioinformatics': ['global-chem-extensions[bioinformatics]'],
         'machine_learning': ['global-chem-extensions[machine_learning]'],
         'pdf': ['global-chem-extensions[pdf]'],
-        'plotting': ['global-chem-extensions[plotting]']
+        'plotting': ['global-chem-extensions[plotting]'],
+        'all': [
+            'global-chem-extensions',
+            'global-chem-extensions[web_server]',
+            'global-chem-extensions[validation]',
+            'global-chem-extensions[bioinformatics]',
+            'global-chem-extensions[machine_learning]',
+            'global-chem-extensions[pdf]',
+            'global-chem-extensions[plotting]'
+        ]
     },
     zip_safe=False,
     keywords='smiles molecules chemistry',
