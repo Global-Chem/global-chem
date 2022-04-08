7faf1c15bfbd50f8fbb77afa52e631d2401147ac
Sulstice <sharifsuliman1@gmail.com>
2022-04-08 14:11:21 -0400

    ss-main: added depedency

--- global_chem/setup.py
+++ global_chem/setup.py
@@ -30,7 +30,7 @@ else:
 # ----
 setup(
     name="global_chem",
-    version="1.5.8",
+    version="1.5.9.3",
     packages=find_packages(),
     license='MPL 2.0',
     author="Suliman Sharif",
@@ -39,6 +39,15 @@ setup(
     install_requires=[],
     long_description=long_description,
     long_description_content_type='text/markdown',
+    extras_require={
+        'extensions': ['global-chem-extensions'],
+        'web_server': ['global-chem-extensions[web_server]'],
+        'validation': ['global-chem-extensions[validation]'],
+        'bioinformatics': ['global-chem-extensions[bioinformatics]'],
+        'machine_learning': ['global-chem-extensions[machine_learning]'],
+        'pdf': ['global-chem-extensions[pdf]'],
+
+    },
     zip_safe=False,
     keywords='smiles molecules chemistry',
     classifiers=[
