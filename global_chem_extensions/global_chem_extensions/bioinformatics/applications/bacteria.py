721d5cb6232ae6f66b7a35ea9201049766efd07a
Sulstice <sharifsuliman1@gmail.com>
2022-04-29 09:53:32 -0400

    ss-master: moving bacteria forward, don't want to lose this code need it in

--- global_chem_extensions/global_chem_extensions/bioinformatics/applications/bacteria.py
+++ global_chem_extensions/global_chem_extensions/bioinformatics/applications/bacteria.py
@@ -1,92 +1,208 @@
-Bacterial Classifiers
-Main phenotypic systems of classification
-•	Classification based on composition of cell wall
-o	Gram-Positive: contain one plasma membrane covered in a thick peptidoglycan cell wall
-	Example: Staphylococcus (genus name)
-o	Gram-Negative: Contain 2 membranes. An inner membrane that separates the cytoplasm from the periplasm and an outer membrane that separates the periplasm from the extracellular space. The outer membrane is covered by a lipopolysaccharide cell wall. Between the two membranes within the periplasm there is a thin peptidoglycan layer.
-	Example: Pseudomonas (genus name)
-•	Classification Based on Shape
-o	Bacillus: Rod-Shaped
-	Example: Escherichia coli (species name)
-o	Spirilla or spirochete: spiral-shaped
-	Example: Spirillum volutans (species name)
-o	Coccus: Sphere-shaped
-	Example: Streptococcus pneumoniae (species name)
-o	Vibrio: Comma-shaped
-	Vibrio cholerae (species name)
-•	Classification based on mode of nutrition
-o	Autotrophic bacteria: derive energy themselves from light or chemical reactions
-	Photoautotrophs: Derive energy from light
-•	Oxygenic photosynthetic bacteria: perform oxygenic photosynthesis to derive energy using H2O as the electron donor and produces oxygen
-o	Example: Cyanobacteria (phylum name)
-•	 Anoxygenic photosynthetic bacteria: Use H2S, H2 or thiosulfate as reducing agent and hydrogen sources to produce energy
-o	Purple sulfur bacteria: Use H2S or thiosulphates as a reducing agent and release sulfur
-	Examples: Chromatiaceae and Ectothiorhodospiraceae (These are the only 2 families of purple sulfur bacteria) 
-o	Purple non-sulfur bacteria: Mainly use H2 as a reducing agent 
-	Examples: All belong to the order Rhodospirillales
-o	Green sulfur bacteria: use sulfide, H2, or Fe(II) as an electron donor.
-	Examples: All belong to the Chlorobiaceae family
-	Chemoautotrophs: Derive energy from inorganic oxidation
-•	Sulfur bacteria: oxidize H2S or thiosulphates to molecular sulfur or sulphates.
-o	Examples: Beggiatoa, Thiobacillus, Thiothrix, Sulfolobus (all are genus names)
-•	Nitrogen bacteria: Nitrifying bacteria convert ammonia to nitrite or nitrite to nitrate to produce energy.
-o	Examples: Nitrosomonas (genus name, converts ammonia to nitrite), Nitrobacter (genus name, converts nitrite to nitrate)
-•	Hydrogen bacteria: Oxidize H2 to produce energy
-o	Examples: Heliobacter pylori, Hydrogenobacter thermophilus, Hydrogenovibrio marinus (all are species names)
-•	Methanotrophs: use methane as a carbon source and to derive energy
-o	Examples: Methylomonas albis, Methylococcus capsulatus (species names)
-•	Iron bacteria: Oxidize ferrous ions to ferric ions to produce energy
-o	Examples: Thiobacillus ferrooxidans, Geobacter metallireducens (species names)
-o	Heterotophic bacteria: Derive energy from organic compounds
-	Parasitic bacteria: derive their food from living organisms, not all are pathogenic
-•	Examples of pathogenic parasitic bacteria:
-o	In plants: Xanthomonas axonopodis, Agrobacterium tumefaciens, Xanthomonas campestris, Pseudomonas syringae, Pseudomonas solanacearum (all are species names)
-o	In Humans: Vibrio cholerae, Salmonella typhi, Clostridium tetani, Bordetella pertussis, Corynebacterium diphtheriae, Escherichia coli, Mycobacterium tuberculosis, Neisseria gonorrhoeae (all are species names)
-	Symbiotic bacteria: Live in a symbiotic association with other organisms
-•	Examples: 
-o	Plants: Rhizobium (genus, found in root nodules)
-o	Animals:  Bacteroidetes (genus, found in gut microbiome)
-	Saprophytic bacteria: Feed on dead or decaying organic matter. Break down complex organic matter into simple compounds and assimilate them.
-•	Spirochaeta cytophaga
- 
-•	Classification based on mode of respiration
-o	Aerobic bacteria: Produce more energy than anaerobic bacteria, uses molecular oxygen as the final electron acceptor.
-	Obligate aerobes: Can only respire aerobically, requires oxygen to survive and are generally killed under anaerobic conditions
-•	Example: Bacillus subtilis (species name)
-	Facultative anaerobes: Generally respire anaerobically but can switch to anaerobic respiration if oxygen becomes deficient
-•	Example: Pseudomonas aeruginosa (species name)
-o	Anaerobic bacteria: Produce less energy than aerobic bacteria, uses CO2, sulfur, fumarate, or Fe(III) as the final electron acceptor
-	Obligate anaerobes: Can only respire anaerobically and are generally killed under aerobic conditions
-•	Example: Clostridium botulinum (species name)
-	Facultative aerobes: Respire anaerobically under normal conditions but can respire aerobically when oxygen is available
-•	Example: Escherichia coli (species name)
-•	Example of phenotypic bacterial classification (this does not include all classifiers)
- 
-Other phenotypic methods of classification
-•	Classification based on environmental reservoirs:
-o	Endogenous reservoirs: found on or within the body
-	Examples: Bacillota and Bacteroidota (genus names, found in the gut)
-o	Exogenous reservoirs: found somewhere in the environment
-	Vibrio cholerae (waterborne) and Salmonella enterica (foodborne) (species names)
-•	Serologic systems for classification: 
-o	Selected antisera can be used to classify different bacterial species. This may be based on either carbohydrate or protein antigens from the bacterial cell wall or the capsular polysaccharide. (Group A streptococcal M proteins or O and H polysaccharide antigens of salmonella).
-Genotypic systems of classification
-•	Universal Phylogenetic Tree:
-o	Used for all living organisms that establishes a tripartite division of all living organisms, bacteria, archaea and eucarya (domains), based on a comparison of 16s ribosomal RNA sequences
-Taxonomic hierarchy:
-•	(least specific) Domain->Phylum->Class->Order->Family->Genus->Species
-•	Specific example of Taxonomic classification (Pseudomonas aeruginosa)
-o	Domain: Bacteria
-o	Phylum: Pseudomonadota
-o	Class: Gammaproteobacteria
-o	Order: Pseudomonadales
-o	Family:	Pseudomonadaceae
-o	Genus:	Pseudomonas
-o	Species: P. aeruginosa
-References
-•	https://byjus.com/biology/bacteria/#classification 
-•	https://byjus.com/neet/autotrophic-bacteria/ 
-•	https://byjus.com/neet/heterotrophic-bacteria/ 
-•	https://byjus.com/biology/difference-between-aerobic-and-anaerobic-bacteria/ 
-•	https://www.biologydiscussion.com/bacteria/respiration-in-bacteria-with-diagram/52066 
-•	http://www.columbia.edu/itc/hs/medical/pathophys/id/2009/introNotes.pdf 
+#!/usr/bin/env python3
+#
+# GlobalChemExtensions - GlobalChem Bacteria
+
+# -----------------------------------------
+
+class Bacteria(Object):
+
+    __version__ = '0.0.1'
+
+    __ALLOWED_CELL_WALL_COMPOSITIONS__ = [
+        'thick_peptidoglycan',
+        'lipopolysaccharide'
+    ]
+
+    __BACTERIA_SHAPES__ = {
+        'bacillus': 'rod',
+        'spirilla': 'spiral',
+        'coccus': 'sphere',
+        'vibrio': 'comma',
+    }
+
+    __NUTRITIONAL_SOURCES__ = {
+        'anoxygenic_photosynthetic' : 'thiosulfate',
+        'autotrophic': 'self',
+        'chemoautotrophs': 'inorganic_oxidation',
+        'green_sulfur': 'sulfide:hydyogen:iron',
+        'oxygenic_photosynthetic': 'oxygenic_photosynthesis',
+        'photoautotrophs': 'light',
+        'purple_sulfur': 'thiosulphates',
+        'purple_non_sulfur': 'hydrogen',
+        'sulfur': 'hydrogen_sulfide:sulphate:thiosulphate',
+        'nitrogen': 'nitrite:nitrate',
+        'hydrogen': 'oxidize_hydrogen',
+        'methanotrophs': 'methane',
+        'iron': 'oxidize_ferrous_ions',
+        'heterotophic': 'organic_compounds',
+        'parasitic': 'living_organisms',
+        'symbiotic': 'symbiotic_association',
+        'saprophytic': 'decaying_organic_matter',
+    }
+
+    __RESPIRATION_METHODS__ = {
+        'aerobic': ' molecular_oxygen',
+        'obligate': 'only_aerobically',
+        'facultative_anaerobes': 'anaerobically:anaerobic',
+        'anaerobic': 'carbon_dioxide:sulphur:fumarate:iron_three',
+    }
+
+    __ENVIRONMENT_RESERVOIRS__ = {
+        'endogenous': 'inside_body',
+        'exogenous': 'outside_body',
+    }
+
+    __SEROLOGIC_SYSTEMS__ = [
+        'carbohydrate',
+        'protein'
+    ]
+
+    __TAXONOMIC_CLASSIFIERS__ = [
+        'domain',
+        'phylum',
+        'class',
+        'order',
+        'family',
+        'genus',
+        'species'
+    ]
+
+    def __init__(
+            self,
+            name = 'staphylococcus',
+            genus_name = '',
+            species_name = '',
+            family = 'chlorobiaceae',
+            cell_wall_composition = 'thick_peptidoglycan',
+            gram_positive = False,
+            gram_negative = False,
+            inner_membrane = None,
+            outer_membrane = None,
+            shape = None,
+            nutrition_type = None,
+            respiratory = None,
+            environment = None,
+            antisera = None,
+            universal_phylogenetic_tree_rna_sequence = None,
+            taxonomic_hierarchy = None,
+
+    ):
+
+        '''
+
+        Arguments:
+            name (String): Name of the Bacteria
+            family (String): Family of the Bacteria
+            genus_name (String): Genus Name of the Bacteria
+            species_name (String): Species Name of the Bacteria where it is derived from
+            cell_wall_composition (String): What the composition of the cell wall is made up of
+            gram_positive (Bool): Typically means one plasma membrane and set as a Bool Flag for user use.
+            gram_negative (Bool): Typically means two plasma membrane and set as a Bool Flag for user use.
+            inner_membrane (String): Seperates the Cytoplasm from the periplasm
+            outer_membrane (String): Seperates the Periplasm from the extracellular space
+            shape (String): Classification based on shape and needs comparison to the allowed shape classifiers
+            nutrition_type (String): the nutritional source of the bacteria to gain energy.
+            environment (String): environment of the bacteria
+            antisera (String): the Serologic environment TODO: What does this mean?
+            universal_phylogenetic_tree_rna_sequence (Dict): RNA Sequence
+            taxonomic_hierarchy (Dict): A hierarchy of the taxonomy
+
+        References:
+            1.) https://byjus.com/biology/bacteria/#classification
+            2.) https://byjus.com/neet/autotrophic-bacteria/
+            3.) https://byjus.com/neet/heterotrophic-bacteria/
+            4.) https://byjus.com/biology/difference-between-aerobic-and-anaerobic-bacteria/
+            5.) https://www.biologydiscussion.com/bacteria/respiration-in-bacteria-with-diagram/52066
+            6.) http://www.columbia.edu/itc/hs/medical/pathophys/id/2009/introNotes.pdf
+
+        '''
+
+
+        # All Variables
+        # --------------
+
+        self.name = name
+        self.family = family
+        self.genus_name = genus_name
+        self.species_name = species_name
+
+        # Phenotype Classifiers
+        # ---------------------
+
+
+        # Based on Cell Wall and Gram-Negative and Gram-Positive
+
+        self.cell_wall_composition = cell_wall_composition
+        self.gram_positive = gram_positive
+        self.gram_negative = gram_negative
+        self.inner_membrane = inner_membrane
+        self.outer_membrane = outer_membrane
+
+        # Based on Shape
+
+        if shape and shape not in list(self.__BACTERIA_SHAPES__.values()):
+            print ("Shape not in the accepted bacteria shape classifiers")
+            raise ValueError
+        else:
+            self.shape = shape
+
+        # Based on Nutrition
+
+        if nutrition_type and nutrition_type not in list(self.__NUTRITIONAL_SOURCES__.values()):
+            print ("Nutrition not in the accepted bacteria shape classifiers")
+            raise ValueError
+        else:
+            self.nutrition_type = nutrition_type
+
+        # Based on Respiratory
+
+        if respiratory and respiratory not in list(self.__RESPIRATION_METHODS__.values()):
+            print ("Respiratory not in the accepted bacteria shape classifiers")
+            raise ValueError
+        else:
+            self.nutrition_type = nutrition_type
+
+        # Based on Environment
+
+        if environment and environment not in list(self.__ENVIRONMENT_RESERVOIRS__.values()):
+            print ("Environment not in the accepted bacteria shape classifiers")
+            raise ValueError
+        else:
+            self.environment = environment
+
+        # Based on Serologic
+
+        if antisera and antisera not in list(self.__SEROLOGIC_SYSTEMS__):
+            print ("Antisera not in the accepted bacteria shape classifiers")
+            raise ValueError
+        else:
+            self.antisera = antisera
+
+        # Based on Tree
+
+        self.universal_phylogenetic_tree_rna_sequence = universal_phylogenetic_tree_rna_sequence
+
+        # Based on Taxonomic Classifiers
+
+        for classifier in self.__TAXONOMIC_CLASSIFIERS__:
+            if classifier not in taxonomic_hierarchy:
+                print ("Missing Classifier: %s" % classifier)
+                raise ValueError
+
+        self.taxonomic_hierarchy = taxonomic_hierarchy
+
+
+if __name__ == '__main__':
+
+    bacteria = Bacteria(
+        name='staphylococcus',
+        cell_wall_composition='thick_peptidoglycan',
+        gram_positive=True,
+    )
+
+    bacteria = Bacteria(
+        name='pseudomonas',
+        cell_wall_composition='lipopolysaccharide',
+        gram_positive=False,
+        gram_negative=True
+    )
