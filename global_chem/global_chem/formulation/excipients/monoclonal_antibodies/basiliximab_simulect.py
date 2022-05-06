0d1dce768dc1ef98123c0253565bc2f80d517bee
asukaorr <51387362+asukaorr@users.noreply.github.com>
2022-05-06 16:14:47 -0400

    Update basiliximab_simulect.py

--- global_chem/global_chem/formulation/excipients/monoclonal_antibodies/basiliximab_simulect.py
+++ global_chem/global_chem/formulation/excipients/monoclonal_antibodies/basiliximab_simulect.py
@@ -18,7 +18,7 @@
              'mannitol': 'C([C@H]([C@H]([C@@H]([C@@H](CO)O)O)O)O)O',
              'sodium phosphate, dibasic, anhydrous': 'OP(=O)([O-])[O-].[Na+].[Na+]',
              'potassium phosphate monobasic': 'OP(=O)(O)[O-].[K+]',
-             'sucrose': 'C([C@@H]1[C@H]([C@@H]([C@H]([C@H](O1)O[C@]2([C@H]([C@@H]([C@H](O2)CO)O)O)CO)O)O)O)O'
+             'sucrose': 'C([C@@H]1[C@H]([C@@H]([C@H]([C@H](O1)O[C@]2([C@H]([C@@H]([C@H](O2)CO)O)O)CO)O)O)O)O',
          }
 
          return smiles
@@ -30,8 +30,8 @@
              'glycine': '',
              'mannitol': '',
              'sodium phosphate, dibasic, anhydrous': '',
-             'potassium phosphate monobasic': ''
-             'sucrose': ''
+             'potassium phosphate monobasic': '',
+             'sucrose': '',
          }
 
          return smarts
