47aebd83142588531b5a957944f8151ec9540c75
asukaorr <51387362+asukaorr@users.noreply.github.com>
2022-05-06 16:12:09 -0400

    Create basiliximab_simulect.py

---
+++ global_chem/global_chem/formulation/excipients/monoclonal_antibodies/basiliximab_simulect.py
@@ -0,0 +1,38 @@
+ #!/usr/bin/env python3
+ #
+ # GlobalChem - Basiliximab Simulect
+ #
+ # -----------------------------------
+
+ class Basiliximab_Simulect(object):
+
+     def __init__(self):
+
+         self.name = 'basiliximab_simulect'
+
+     @staticmethod
+     def get_smiles():
+
+         smiles = {
+             'glycine': 'C(C(=O)O)N',
+             'mannitol': 'C([C@H]([C@H]([C@@H]([C@@H](CO)O)O)O)O)O',
+             'sodium phosphate, dibasic, anhydrous': 'OP(=O)([O-])[O-].[Na+].[Na+]',
+             'potassium phosphate monobasic': 'OP(=O)(O)[O-].[K+]',
+             'sucrose': 'C([C@@H]1[C@H]([C@@H]([C@H]([C@H](O1)O[C@]2([C@H]([C@@H]([C@H](O2)CO)O)O)CO)O)O)O)O'
+             '
+         }
+
+         return smiles
+
+     @staticmethod
+     def get_smarts():
+
+         smarts = {
+             'glycine': '',
+             'mannitol': '',
+             'sodium phosphate, dibasic, anhydrous': '',
+             'potassium phosphate monobasic': ''
+             'sucrose': ''
+         }
+
+         return smarts
