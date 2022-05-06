65d123da5d2d2066c0edfede4b93581c51bbb15b
asukaorr <51387362+asukaorr@users.noreply.github.com>
2022-05-06 18:02:48 -0400

    Create trastuzumab_kanjinti.py

---
+++ global_chem/global_chem/formulation/excipients/monoclonal_antibodies/trastuzumab_kanjinti.py
@@ -0,0 +1,35 @@
+ #!/usr/bin/env python3
+ #
+ # GlobalChem - Trastuzumab Kanjinti
+ #
+ # -----------------------------------
+
+ class Trastuzumab_Kanjinti(object):
+
+     def __init__(self):
+
+         self.name = 'trastuzumab_kanjinti'
+
+     @staticmethod
+     def get_smiles():
+
+         smiles = {
+             'histidine': 'C1=C(NC=N1)C[C@@H](C(=O)O)N',
+             'histidine hydrochloride monohydrate': 'C1=C(NC=N1)C[C@@H](C(=O)O)N.O.Cl',
+             'polysorbate 20': 'CCCCCCCCCCCC(=O)OCCOCC(C1C(C(CO1)OCCO)OCCO)OCCO',
+             'trehalose': 'C([C@@H]1[C@H]([C@@H]([C@H]([C@H](O1)O[C@@H]2[C@@H]([C@H]([C@@H]([C@H](O2)CO)O)O)O)O)O)O)O',
+         }
+
+         return smiles
+
+     @staticmethod
+     def get_smarts():
+
+         smarts = {
+             'histidine': ''
+             'histidine hydrochloride monohydrate': ''
+             'polysorbate 20': '',
+             'trehalose': 'C([C@@H]1[C@H]([C@@H]([C@H]([C@H](O1)O[C@@H]2[C@@H]([C@H]([C@@H]([C@H](O2)CO)O)O)O)O)O)O)O',
+         }
+
+         return smarts
