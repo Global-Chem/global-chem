a4dc64164c3773c98a6e88eb944d620fde1f2502
Suliman Sharif <sharifsuliman1@gmail.com>
2022-05-08 07:55:28 -0400

    Update basiliximab_simulect.py

--- global_chem/global_chem/formulation/excipients/monoclonal_antibodies/basiliximab_simulect.py
+++ global_chem/global_chem/formulation/excipients/monoclonal_antibodies/basiliximab_simulect.py
@@ -4,7 +4,7 @@
  #
  # -----------------------------------
 
- class Basiliximab_Simulect(object):
+ class BasiliximabSimulect(object):
 
      def __init__(self):
 
@@ -15,10 +15,10 @@
 
          smiles = {
              'glycine': 'C(C(=O)O)N',
-             'mannitol': 'C([C@H]([C@H]([C@@H]([C@@H](CO)O)O)O)O)O',
+             'mannitol': 'C(C(C(C(C(CO)O)O)O)O)O',
              'sodium phosphate, dibasic, anhydrous': 'OP(=O)([O-])[O-].[Na+].[Na+]',
              'potassium phosphate monobasic': 'OP(=O)(O)[O-].[K+]',
-             'sucrose': 'C([C@@H]1[C@H]([C@@H]([C@H]([C@H](O1)O[C@]2([C@H]([C@@H]([C@H](O2)CO)O)O)CO)O)O)O)O',
+             'sucrose': 'C(C1C(C(C(C(O1)OC2(C(C(C(O2)CO)O)O)CO)O)O)O)O',
          }
 
          return smiles
