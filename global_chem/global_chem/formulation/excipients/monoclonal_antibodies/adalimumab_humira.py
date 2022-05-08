8b117a7f47d370a74725d327181b096c5e757099
Suliman Sharif <sharifsuliman1@gmail.com>
2022-05-08 07:54:21 -0400

    Update adalimumab_humira.py

--- global_chem/global_chem/formulation/excipients/monoclonal_antibodies/adalimumab_humira.py
+++ global_chem/global_chem/formulation/excipients/monoclonal_antibodies/adalimumab_humira.py
@@ -4,7 +4,7 @@
  #
  # -----------------------------------
 
- class Adalimumab_Humira(object):
+ class AdalimumabHumira(object):
 
      def __init__(self):
 
@@ -14,10 +14,9 @@
      def get_smiles():
 
          smiles = {
-             'mannitol': 'C([C@H]([C@H]([C@@H]([C@@H](CO)O)O)O)O)O',
-             'polysorbate 80': 'CCCCCCCC/C=C/CCCCCCCC(=O)OCC(C1C(C(CO1)O)O)O',
-             'sucrose': 'C([C@@H]1[C@H]([C@@H]([C@H]([C@H](O1)O[C@]2([C@H]([C@@H]([C@H](O2)CO)O)O)CO)O)O)O)O'
-             '
+             'mannitol': 'C(C(C(C(C(CO)O)O)O)O)O',
+             'polysorbate 80': r'CCCCCCCC/C=C/CCCCCCCC(=O)OCC(C1C(C(CO1)O)O)O',
+             'sucrose': 'C(C1C(C(C(C(O1)OC2(C(C(C(O2)CO)O)O)CO)O)O)O)O'
          }
 
          return smiles
