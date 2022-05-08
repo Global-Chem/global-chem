49c4c6227b3ed184a05e3ce72a16a248f73cbfa5
Betty <104396477+LierB@users.noreply.github.com>
2022-05-08 14:37:39 +0200

    Update named_reactions_in_organic_synthesis.py

--- global_chem/global_chem/organic_synthesis/named_reactions_in_organic_synthesis/named_reactions_in_organic_synthesis.py
+++ global_chem/global_chem/organic_synthesis/named_reactions_in_organic_synthesis/named_reactions_in_organic_synthesis.py
@@ -37,7 +37,7 @@ class NamedReactionsInOrganicSynthesis(object):
         'malonic_ester_synthesis': 'O=C(CC(OC)=O)OC.O=C(OC)CC',
         'pinner_reaction': 'CC#N.CC(OC)=N',
         'koenigs-knorr_glycosidation': 'CC(OCC1C(OC(C)=O)C(OC(C)=O)C(OC(C)=O)C(Br)O1)=O.CC(OCC2C(OC(C)=O)C(OC(C)=O)C(OC(C)=O)C(OC)O2)=O',
-        'skraup_and_doebner-miller_reaction': 'NC1=CC=CC=C1.O=C/C=C/C.CC2=NC3=CC=CC=C3C=C2',
+        'skraup_and_doebner-miller_reaction': r'NC1=CC=CC=C1.O=C/C=C/C.CC2=NC3=CC=CC=C3C=C2',
         'ciamician-dennstedt_rearrangement': 'C1=CNC=C1.[Cl,Br,I]C2=CC=CN=C2',
         'fries-_photo-fries_rearrangement': 'O=C(C)OC1=CC=CC=C1.OC2=CC=C(C(C)=O)C=C2',
 	'anionic_ortho-fries_rearrangement': 'O=C(C)OC1=CC=CC=C1.O=C(C)C2=C(O)C=CC=C2',
@@ -59,7 +59,7 @@ class NamedReactionsInOrganicSynthesis(object):
         'knorr_pyrrole_synthesis': 'O=C(C(C)N)C.O=C(OCC)CC(C)=O.CC1=C(C(OCC)=O)C(C)=C(C)N1',
         'claisen_condensation': 'CCOC(C)=O.CCOC(CC(C)=O)=O',
         'gabriel_synthesis': 'C[F,Cl,Br,I].CN',
-        'japp-klingemann_reaction': '[N+]=NC1=CC=CC=C1.CC(C(C)C(O)=O)=O.C/C(C(OC)=O)=N\NC1=CC=CC=C1',
+        'japp-klingemann_reaction': r'[N+]=NC1=CC=CC=C1.CC(C(C)C(O)=O)=O.C/C(C(OC)=O)=N\NC1=CC=CC=C1',
         'reformatsky_reaction': '[F,Cl,Br,I]CC(OC)=O.C=O.COC(CCO)=O',
         'tishchenko_reaction': 'CC=O.CC(OCC)=O',
         'dimroth_rearrangement': 'NC1=C(C)N=NN1C.CC2=C(NC)NN=N2',
@@ -68,20 +68,20 @@ class NamedReactionsInOrganicSynthesis(object):
         'bischler-napieralski_isoquinoline_synthesis': 'CC(NCCC1=CNC2=CC=CC=C21)=O.CC3=NCCC4=C3NC5=CC=CC=C54',
         'dienone-phenol_rearrangement': 'CC(C=C1)(C)C=CC1=O.CC2=C(C)C=C(O)C=C2',
         'pomeranz-fritsch_reaction': 'O=CC1=CC=CC=C1.NCC(OC)OC.C23=CC=CC=C2C=NC=C3',
-        'stobbe_condensation': 'CCOC(CCC(OCC)=O)=O.CC(C)=O.C/C(C)=C(CC(O)=O)\C(OCC)=O',
+        'stobbe_condensation': r'CCOC(CCC(OCC)=O)=O.CC(C)=O.C/C(C)=C(CC(O)=O)\C(OCC)=O',
         'favorskii_rearrangement_and_homo-favorskii_rearrangement': '[F,Cl,Br,I]C1CCCCC1=O.O=C(O)C2CCCC2',
-        'knoevenagel_condensation': 'O=CC1=CC=CC=C1.COC(CC(OC)=O)=O.O=C(OC)/C(C(OC)=O)=C/C2=CC=CC=C2',
+        'knoevenagel_condensation': r'O=CC1=CC=CC=C1.COC(CC(OC)=O)=O.O=C(OC)/C(C(OC)=O)=C/C2=CC=CC=C2',
         'nef_reaction': 'O=[N]([O])C(C)C.O=C(C)C',
         'smiles_rearrangement': '[F,Cl,Br,I]C1=C(OCCN)C=CC([N+]([O-])=O)=C1.[F,Cl,Br,I]C2=C(NCCO)C=CC([N+]([O-])=O)=C2',	# X-Y rearrangment where X(here O) and Y(here NH2) can be many different groups, also ortho- and para- substituents(here halogens and NO2) can be different groups
         'wacker_oxidation': 'C=C.CC=O',
         'henry_reaction': 'CC[N+]([O-])=O.CC(C)=O.C/C([N+]([O-])=O)=C(C)\C',	# from dehydration, CC(C(C)=C)[N+]([O-])=O from oxidation, CC(C(O)(C)C)N from reduction
         'michaelis-arbuzov_reaction': 'CP(C)OC.CP(C)(C)=O',	# phosphinite to phosphine, CP(OC)OC.CP(C)(OC)=O phosphonite to phosphinate, COP(OC)OC.CP(OC)(OC)=O phosphite ester to phosphonate oxide
         'gattermann_and_gattermann-koch_formylation': 'C#N.O=CC1=CC=CC=C1',
-        'chugaev_elimination_xanthate_ester_pyrolysis': 'CC(C)C(C)(C)O.C/C(C)=C(C)/C',
+        'chugaev_elimination_xanthate_ester_pyrolysis': r'CC(C)C(C)(C)O.C/C(C)=C(C)/C',
         'baeyer-villiger_oxidation': 'CC(C)=O.CC(OO)=O.CC(OC)=O',
         'barbier_coupling_reaction': 'C[Cl,Br,I].CC(C)=O.CC(O)(C)C',
         'prins_reaction': 'CC=C.CC(C)=O.OC(C)CCO',	# to 1,3 diol, C/C=C/CO allylic alcohol in absence of water, CC1OCOCC1 dioxane with excess of CC(C)=O
-        'wagner-meerwein_rearrangement': 'CCC[F,Cl,Br,I].C/C=C/C',	# or to CC([F,Cl,Br,I])C (hydrid shift)
+        'wagner-meerwein_rearrangement': r'CCC[F,Cl,Br,I].C/C=C/C',	# or to CC([F,Cl,Br,I])C (hydrid shift)
         'grignard_reaction': 'C=O.C[Mg]Br.CCO',	# to primary alcohol, other aldehyds to secondary alcohols CC=O.CC(O)C, ketones to tertiary alcohols CC(C)=O.CC(C)(O)C,…  
         'demjanov_rearrangement': 'NCC1CCC1.OC2CCCC2',	# ring expansion, or ring reduction NC1CCCC1.OCC2CCC2
 	'tiffeneau-demjanov_rearrangement': 'OC1(CN)CCCC1.O=C2CCCCC2',
@@ -90,36 +90,36 @@ class NamedReactionsInOrganicSynthesis(object):
         'wolff_rearrangement': 'O=C(C)C(C)=[N+]=[N-].OC(C(C)C)=O',
         'benzoin_and_retro-benzoin_condensation': 'O=CC1=CC=CC=C1.O=C(C(O)C2=CC=CC=C2)C3=CC=CC=C3',
         'mannich_reaction': 'CNC.C=O.CC(C(C)=O)C.CN(CC(C)(C(C)=O)C)C',
-        'nazarov_cyclization': 'C/C=C/C(/C=C/C)=O.O=C1C=C(C)C(C)C1',
+        'nazarov_cyclization': r'C/C=C/C(/C=C/C)=O.O=C1C=C(C)C(C)C1',
         'ullmann_biaryl_ether_and_biaryl_amine_synthesis_condensation': '[F,Cl,Br,I]C1=CC=CC=C1.C2(C3=CC=CC=C3)=CC=CC=C2',
         'eschweiler-clarke_methylation': 'CNC.O=C.OC=O.CN(C)C',
-        'staudinger_ketene_cycloaddition': 'C/C(C)=N\C.CC(C)=C=O.CN1C(C)(C)C(C)(C)C1=O',
+        'staudinger_ketene_cycloaddition': r'C/C(C)=N\C.CC(C)=C=O.CN1C(C)(C)C(C)(C)C1=O',
         'acetoacetic_ester_synthesis': 'CC(CC(OC)=O)=O.CC(CC)=O',
         'dakin_oxidation': 'OC1=CC=C(C=O)C=C1.OC2=CC=C(C(C)=O)C=C2.OC3=CC=C(O)C=C3',
-        'paterno-büchi_reaction': 'CC(C)=O.C/C(C)=C(C)\C.CC1(C)C(C)(C)C(C)(C)O1',	# stereochemistry when R groups are different on the 2nd reactant
-        'prilezhaev_reaction': 'C/C(C)=C(C)\C.CC(OO)=O.CC1(C)OC1(C)C',
+        'paterno-büchi_reaction': r'CC(C)=O.C/C(C)=C(C)\C.CC1(C)C(C)(C)C(C)(C)O1',	# stereochemistry when R groups are different on the 2nd reactant
+        'prilezhaev_reaction': r'C/C(C)=C(C)\C.CC(OO)=O.CC1(C)OC1(C)C',
         'pummerer_rearrangement': 'CS(CC)=O.CC(OC(C)=O)=O.CSC(OC(C)=O)C',
         'finkelstein_reaction': 'C[F,Br,I].[Cl-].CCl',	# halogen replacement with any combination 
         'regitz_diazo-transfer_reaction': 'CC(CC)=O.[N]=[N+]=[N-].CC(C(C)=[N+]=[N-])=O',
         'pictet-spengler_tetrahydroisoquinoline_synthesis': 'NCCC1=CC=CC=C1.CC=O.CC2C3=CC=CC=C3CCN2',	# from Phenylethylamine, NCCC1=CNC2=CC=CC=C21.CC=O.CC(NCC3)C4=C3C5=CC=CC=C5N4 from Tryptamine
         'wolff-kishner_reduction': 'CC(C)=O.CCC',
         'madelung_indole_synthesis': 'CCC1=CC=CC=C1NC(C)=O.CC2=C(C)NC3=CC=CC=C32',
-        'claisen_rearrangement': 'C=CCO/C(C)=C/C.O=C(C(CC=C)C)C',
+        'claisen_rearrangement': r'C=CCO/C(C)=C/C.O=C(C(CC=C)C)C',
         'clemmensen_reduction': 'CC(C)=O.CCC',	# reduction with Zn		
-        'wharton_olefin_synthesis': 'O=C(C)C1OC1C.C/C=C\C(C)O',
+        'wharton_olefin_synthesis': r'O=C(C)C1OC1C.C/C=C\C(C)O',
         'chichibabin_amination_reaction': 'CC1=CC=CC=N1.CC2=CC=CC(N)=N2',
         'ferrier_rearrangement_reaction': 'O=C(C)OC1C(COC(C)=O)OC=CC1OC(C)=O.O=C(C)OC2C(COC(C)=O)OC(OC)C=C2',	
 	'ferrier_II_carbocyclization_reaction': 'C=C1C(OC)C(OC)C(OC)C(OC)O1.O=C2C(OC)C(OC)C(OC)C(O)C2', 
         'houben-hoesch_reaction': 'CC#N.OC1=CC(O)=CC=C1.OC2=CC(O)=CC=C2C(C)=O',
-        'aza-wittig_reaction': 'CP(C)(C)=NC.CC(C)=O.C/C(C)=N/C',	# with ketone to imine, with CO2 to isocyanate, with CS2 to isothiocyanate, with H2O to primary amine (=staudinger),…
+        'aza-wittig_reaction': r'CP(C)(C)=NC.CC(C)=O.C/C(C)=N/C',	# with ketone to imine, with CO2 to isocyanate, with CS2 to isothiocyanate, with H2O to primary amine (=staudinger),…
         'meisenheimer_rearrangement': '[O-][N+](C)(C)C.CN(OC)C',	# [1,2] rearrangement, [O-][N+](C)(C(C)C=C)C.CN(OC/C=C/C)[2,3] rearrangement
         'staudinger_reaction': 'CP(C)(C)=NC.CN',	# = aza-wittig with H2O
-        'wittig_reaction': 'C[P+](C)([C-](C)C)C.CC(C)=O.C/C(C)=C(C)\C',
+        'wittig_reaction': r'C[P+](C)([C-](C)C)C.CC(C)=O.C/C(C)=C(C)\C',
         'wohl-ziegler_bromination': 'C1CCC=CC1.BrC2C=CCCC2',
         'passerini_multicomponent_reaction': 'OC(C)=O.CC(C)=O.[C-]#[N+]C.CC(OC(C)(C)C(N(C)C)=O)=O',
-        'meyer-schuster_and_rupe_rearrangement': 'CC(CC)(C#C)O.C/C(CC)=C\C=O',	# mayer-schuster (1,3 shift of hydroxyl group), rupe (1,2 shift of hydroxyl group) to C/C(C(C)=O)=C/C 
+        'meyer-schuster_and_rupe_rearrangement': r'CC(CC)(C#C)O.C/C(CC)=C\C=O',	# mayer-schuster (1,3 shift of hydroxyl group), rupe (1,2 shift of hydroxyl group) to C/C(C(C)=O)=C/C 
         'schmidt_reaction': 'CC(C)=O.[H][N-][N+]#N.CC(N(C))=O',	# with ketones to amides, with carboxylic acids to amines (CC(O)=O.[H][N-][N+]#N.CN)
-        'amadori_reaction_rearrangement': 'OC(/C=N/C)C.CC(CNC)=O',
+        'amadori_reaction_rearrangement': r'OC(/C=N/C)C.CC(CNC)=O',
         'meerwein-ponndorf-verley_reduction': 'CC(C)=O.OC(C)C',	# reverse reaction of oppenauer_oxidation
         'stephen_aldehyde_synthesis': 'CC#N.CC=O',
         'diels-alder_cycloaddition': 'C=CC=C.C=C.C1=CCCCC1',
@@ -127,23 +127,23 @@ class NamedReactionsInOrganicSynthesis(object):
         'balz-schiemann_reaction': 'NC1=CC=CC=C1.FC2=CC=CC=C2',
         'dakin-west_reaction': 'OC(C(C)N)=O.CC(C(C)NC(C)=O)=O',
         'stevens_rearrangement': 'CC[N+](C)(C)C.CC(N(C)C)C',	# quaternary ammonium salts to amines or CC[S+](C)C.CC(SC)C sulfonium salts to sulfides 
-        'nenitzescu_indole_synthesis': '.O=C1C=CC(C=C1)=O.O=C(OC)/C=C(C)/NC.OC2=CC=C(N(C)C(C)=C3C(OC)=O)C3=C2‘,
+        'nenitzescu_indole_synthesis': r'O=C1C=CC(C=C1)=O.O=C(OC)/C=C(C)/NC.OC2=CC=C(N(C)C(C)=C3C(OC)=O)C3=C2‘,
         'criegee_oxidation': 'OCCO.C=O',
         'riley_selenium_dioxide_oxidation': 'CCC(CC)=O.CCC(C(C)=O)=O',	# R2: CC(C(CC)=O)=O oxidation of methylene groups adjacent to carbonyls
         'baker-venkataraman_rearrangement': 'CC(OC1=CC=CC=C1C(CC)=O)=O.OC2=CC=CC=C2C(C(C)C(C)=O)=O',
-        'prévost_reaction': 'C/C=C\C.OC(C(C)O)C',
+        'prévost_reaction': r'C/C=C\C.OC(C(C)O)C',
         'arndt-eistert_homologation': 'CC(O)=O.OC(CC)=O',
         'payne_rearrangement': 'CC1C(CO)O1.CC(O)C2OC2',
-        'robinson_annulation': 'O=C(C(C)C)C(C)C.O=C(/C(C)=C(C)/C)CC.O=C1C(C)C(C)(C)C(C)(C)C(C(C)C)=C1C',
+        'robinson_annulation': r'O=C(C(C)C)C(C)C.O=C(/C(C)=C(C)/C)CC.O=C1C(C)C(C)(C)C(C)(C)C(C(C)C)=C1C',
         'claisen-ireland_rearrangement': 'C=CCOC(C)=O.OC(CCC=C)=O',
         'oppenauer_oxidation': 'OC(C)C.CC(C)=O', # reverse reaction of meerwein-ponndorf-verley_reduction
         'sommelet-hauser_rearrangement': 'C[N+](C)(C)CC1=CC=CC=C1.CC2=CC=CC=C2CN(C)C',
-        'meerwein_arylation': 'N#[N+]C1=CC=CC=C1.C/C(C2=CC=CC=C2)=C(C)\[N+]([O-])=O.C/C=C([N+]([O-])=O)/C', # instead of NO2 also other EWGs possible
+        'meerwein_arylation': r'N#[N+]C1=CC=CC=C1.C/C(C2=CC=CC=C2)=C(C)\[N+]([O-])=O.C/C=C([N+]([O-])=O)/C', # instead of NO2 also other EWGs possible
         'quasi-favorskii_rearrangement': 'CC([Cl,Br])C(C(C)(C)C)=O.CC(C(C)(C)C)C(OC)=O',
-        'snieckus_directed_ortho_metalation': '',
-        'carrol_rerrangement': '',
-        'cope_rearrangement': '',
-        'ramberg–bäcklund_rearrangement': '',
+        'snieckus_directed_ortho_metalation': 'O=C(N(C)C)C1=CC=CC=C1.O=C(N(C)C)C2=C([Li])C=CC=C2',
+        'carroll_rerrangement': 'CC(CC(OCC=C)=O)=O.CC(CCC=C)=O',
+        'cope_rearrangement': r'C=CCC(C)C=C.C=CCC/C=C/C',
+        'ramberg–bäcklund_rearrangement': r'CCS(C(Br)C)(=O)=O.C/C=C\C',
         'wittig-[1,2]-_and_[2,3]-rearrangement': '',
         'alder_reaction': '',
         'hetero_diels-alder_reaction': 'C=CC=C.C1=CCCCC1',
