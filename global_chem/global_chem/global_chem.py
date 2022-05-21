21f49fe9f88a6446006fe75931e9281e0c54155c
Suliman Sharif <sharifsuliman1@gmail.com>
2022-05-21 10:38:26 -0400

    Update global_chem.py

--- global_chem/global_chem/global_chem.py
+++ global_chem/global_chem/global_chem.py
@@ -7,8 +7,15 @@
 # Base Imports
 
 import os
+import sys
 import os.path
 import pprint
+
+# Reconfigurations
+# ----------------
+
+sys.stdin.reconfigure(encoding='utf-8')
+sys.stdout.reconfigure(encoding='utf-8')
 
 # Environment
 
