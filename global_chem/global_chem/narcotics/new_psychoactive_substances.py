24f799056a91e3d044eb619cf9221b614c1c0c9d
thedevp <thedevpw@gmail.com>
2023-10-24 15:15:18 +0200

    add NPS nodein narcotice

---
+++ global_chem/global_chem/narcotics/new_psychoactive_substances.py
@@ -0,0 +1,10 @@
+
+class EuropeanUnionPsychoactiveSubstancesDrugReport(object):
+
+    def __init__(self):
+        self.name = 'european_union_psychoactive_substances_drug_report'
+
+    @staticmethod
+    def get_smiles():
+         smiles = {}
+         return smiles
