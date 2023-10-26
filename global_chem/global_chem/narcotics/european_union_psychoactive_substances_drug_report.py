9b5598f9bbf748b25457e8e6fbb6b7bb1ecf507e
thedevp <thedevpw@gmail.com>
2023-10-26 09:21:44 +0200

    change .py file name and add node EU and subnode for nps report

---
+++ global_chem/global_chem/narcotics/european_union_psychoactive_substances_drug_report.py
@@ -0,0 +1,12 @@
+
+class EuropeanUnion(object):
+
+    class PsychoactiveSubstancesDrugReport(object):
+        
+        def __init__(self):
+            self.name = 'european_union_psychoactive_substances_drug_report'
+
+        @staticmethod
+        def get_smiles():
+            smiles = {}
+            return smiles
