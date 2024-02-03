343e3aee8c3fdf46fb4d1b877c247f2576ee4f9d
Anuththara Gamage(Anu) <90089876+ANUGAMAGE@users.noreply.github.com>
2024-02-03 06:38:22 +0000

    Molecule_dataframe,plotly.graph_objects and chemicak_questionaire was improved

--- global_chem_extensions/global_chem_extensions/cheminformatics/applications/chemical_questionaire.py
+++ global_chem_extensions/global_chem_extensions/cheminformatics/applications/chemical_questionaire.py
@@ -9,85 +9,87 @@
 
 import pandas as pd
 from rdkit import Chem
-import plotly.graph_objects as go
+import pandas as pd
+from rdkit import Chem
 from global_chem import GlobalChem
 
 class ChemicalQuestionaire(object):
 
+  '''
+
+  Chemical Questionaire to Produce Relations
+
+  '''
+
+  def __init__(
+    self,
+    smiles_list=[],
+    aromaticity=True,
+    element_diversity=True,
+    functional_group_diversity=True,
+    stereoisomer=True,
+    global_chem_node='organic_and_inorganic_bronsted_acids'
+  ):
+
+    self.smiles_list = self.test_smiles(smiles_list)
+    self.aromaticity = aromaticity
+    self.element_diversity = element_diversity
+    self.functional_group_diversity = functional_group_diversity
+    self.stereoisomer = stereoisomer
+    self.global_chem_node = self.get_global_chem_patterns(global_chem_node)
+
+  def test_smiles(self, smiles_list):
+
     '''
 
-    Chemical Questionaire to Produce Relations
+    Test the SMILES
 
     '''
 
-    def __init__(
-      self,
-      smiles_list = [],
-      aromaticity=True,
-      element_diversity=True,
-      functional_group_diversity=True,
-      stereoisomer=True,
-      global_chem_node='organic_and_inorganic_bronsted_acids'
-    ):
+    validate_smiles = []
 
-      self.smiles_list = self.test_smiles(smiles_list)
-      self.aromaticity = aromaticity
-      self.element_diversity = element_diversity
-      self.functional_group_diversity = functional_group_diversity
-      self.stereoisomer = stereoisomer
-      self.global_chem_node = self.get_global_chem_patterns(global_chem_node)
+    for molecule in smiles_list:
 
-    def test_smiles(self, smiles_list):
+      try:
+        validate_smiles.append(Chem.MolFromSmiles(molecule))
+      except:
+        print('SMILES Not Accepted: %s' % molecule)
 
-        '''
+    return validate_smiles
 
-        Test the SMILES
+  def get_global_chem_patterns(self, global_chem_node):
 
-        '''
+    '''
 
-        validate_smiles = []
+    Get the GlobalChem SMILES
 
-        for molecule in smiles_list:
+    '''
 
-            try:
-              validate_smiles.append(Chem.MolFromSmiles(molecule))
-            except:
-              print ('SMILES Not Accepted: %s' % molecule)
+    gc = GlobalChem()
+    gc.build_global_chem_network()
 
-        return validate_smiles
+    node_items = gc.get_node_smiles(global_chem_node)
+    return node_items
 
-    def get_global_chem_patterns(self, global_chem_node):
+  def determine_questions(self):
 
-      '''
+    '''
 
-      Get the GlobalChem SMILES
+    Set-up the dataframe
 
-      '''
+    '''
 
-      gc = GlobalChem()
-      gc.build_global_chem_network()
+    molecule_dataframe = pd.DataFrame()
+    # Access molecule_dataframe here
 
-      node_items = gc.get_node_smiles(global_chem_node)
-      return node_items
-
-    def determine_questions(self):
-
-      '''
-
-      Set-up the dataframe
-
-      '''
-
-      molecule_dataframe = pd.DataFrame()
-      
-
-
-    def plot_data(self):
+  def plot_data(self):
+    # Access plotly.graph_objects here
+    pass
 
 
 if __name__ == '__main__':
-
-    chemical_questionaire = ChemicalQuestionaire()
+  chemical_questionaire = ChemicalQuestionaire()
+  # Access chemical_questionaire here
 
 
 
